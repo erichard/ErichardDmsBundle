c23d99f12e5f9610cb5932b3c4946c4b25c44fde
Erwan Richard <erwann.richard@gmail.com>
2013-05-14 17:43:56 +0200

    Add an import command

---
+++ Command/ImportDocumentCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Erichard\DmsBundle\Command;
+
+use Erichard\DmsBundle\Entity\Document;
+use Erichard\DmsBundle\Entity\DocumentNode;
+use Erichard\DmsBundle\Import\FilesystemImporter;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportDocumentCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('dms:document:import')
+            ->setDescription('Import a document tree into the DMS.')
+            ->addArgument('source', InputArgument::REQUIRED, 'From where the document will be imported.')
+            ->addOption('exclude', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Exclude some files or directories.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $sourceDir = $input->getArgument('source');
+        $manager   = $this->getContainer()->get('doctrine')->getManager();
+
+        // Prepare the destination node
+        $dest = new DocumentNode();
+        $dest->setName('Imported on '. date('Y-m-d') . ' at '. date('H:i'));
+        $manager->persist($dest);
+        $manager->flush();
+
+        // Launch the importer
+        $importer = new FilesystemImporter($this->getContainer()->get('doctrine')->getManager(), array(
+            'storage_path' => $this->getContainer()->getParameter('dms.storage.path')
+        ));
+        $importer->import($sourceDir, $dest, $input->getOption('exclude'));
+    }
+}
---
+++ Import/FilesystemImporter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Erichard\DmsBundle\Import;
+
+use Doctrine\ORM\EntityManager;
+use Erichard\DmsBundle\DocumentNodeInterface;
+use Erichard\DmsBundle\Entity\Document;
+use Erichard\DmsBundle\Entity\DocumentNode;
+use Symfony\Component\Finder\Finder;
+
+class FilesystemImporter
+{
+    protected $em;
+    protected $options;
+
+    public function __construct(EntityManager $em, array $options = array())
+    {
+        $this->em = $em;
+        $this->options = $options;
+    }
+
+    public function import($sourceDir, DocumentNodeInterface $targetNode, array $excludes = array())
+    {
+        if (!is_dir($sourceDir)) {
+            throw new \InvalidArgumentException(sprintf('The directory %s does not exist', $sourceDir));
+        }
+
+        $manager = $this->em;
+        $currentNode = array( 0 => $targetNode );
+
+        $finder = new Finder();
+        $files = $finder->in($sourceDir)->getIterator();
+
+        foreach ($files as $file) {
+            $depth = $files->getDepth();
+            if ($file->isDir()) {
+                $node = new DocumentNode();
+                $node
+                    ->setParent($currentNode[$depth])
+                    ->setName($file->getBaseName())
+                ;
+                $manager->persist($node);
+                $currentNode[$depth+1] = $node;
+            } elseif ($file->isFile()) {
+                $document = new Document($currentNode[$depth]);
+                $document
+                    ->setName($file->getBaseName())
+                    ->setOriginalName($file->getBaseName())
+                    ->setFilename($file->getBaseName())
+                ;
+                $manager->persist($document);
+                $manager->flush();
+
+                $document->setFilename($document->getComputedFilename());
+
+                $destFile = $this->options['storage_path'] . '/' . $document->getFilename();
+
+                if (!is_dir(dirname($destFile))) {
+                    mkdir(dirname($destFile), 0755, true);
+                }
+                copy($file->getRealPath(), $destFile);
+
+                $manager->persist($document);
+            }
+
+            $manager->flush();
+        }
+    }
+}
