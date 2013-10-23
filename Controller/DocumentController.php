dbf5a20ca82f2892077ed23cf6324597db4ecf53
Erwan Richard <erwann.richard@gmail.com>
2013-10-23 10:23:47 +0200

    double namespace

--- Controller/DocumentController.php
+++ Controller/DocumentController.php
@@ -14,7 +14,6 @@ use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Finder\Finder;
 
 class DocumentController extends Controller
 {
